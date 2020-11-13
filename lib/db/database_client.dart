06998e2ecf8f59681269c85ac7c589451ad08209
huangwei <harw.op@qq.com>
2020-11-13 15:06:54 +0800

    database finished

--- lib/db/database_client.dart
+++ lib/db/database_client.dart
@@ -5,4 +5,30 @@ import 'package:flutter_todo_app/db/table/category_table.dart';
 import 'package:flutter_todo_app/db/table/todo_table.dart';
 import 'package:flutter_todo_app/db/todo_gateway.dart';
 
-class TodoClient {}
+class TodoClient {
+  static const version = 1;
+  static const databaseFileName = "todo_database.db";
+
+  static Database _databaseStored;
+
+  Future<Database> get _database async =>
+      _databaseStored ??= await _openDataBase();
+
+  Future<Database> _openDataBase() async {
+    final path = join(await getDatabasesPath(), databaseFileName);
+    return openDatabase(path, version: version,
+        onConfigure: (Database db) async {
+      await db.execute('PRAGMA foreign_keys = ON');
+    }, onCreate: (Database db, int version) async {
+      await CategoryTable.create(db, version);
+      await TodoTable.create(db, version);
+    });
+  }
+
+  Future<void> close() async => _databaseStored.close();
+
+  Future<TodoGateway> getGateway() async {
+    final database = await _database;
+    return TodoGateway(database);
+  }
+}
--- lib/db/todo_gateway.dart
+++ lib/db/todo_gateway.dart
@@ -9,36 +9,91 @@ class TodoGateway {
 
   ///region category operation methods
 
+  Future<List<CategoryTable>> fetchCategories() async {
+    return _fetchCategories(database);
+  }
+
+  Future<void> upsertCategories(List<CategoryTable> categories) async {
+    await database.transaction((txn) async {
+      final batch = txn.batch();
+      for (final category in categories) {
+        if (await _existsCategory(txn, category)) {
+          await _updateCategory(txn, category);
+        } else {
+          await _createCategory(txn, category);
+        }
+      }
+      await batch.commit();
+    });
+  }
+
+  Future<void> deleteCategories(List<CategoryTable> categories) async {
+    await database.transaction((txn) async {
+      final batch = txn.batch();
+      for (final category in categories) {
+        await txn.delete(CategoryTable.tableName,
+            where: '${CategoryTable.tableName} = ?',
+            whereArgs: <String>[category.id]);
+      }
+      await batch.commit();
+    });
+  }
+
   Future<List<CategoryTable>> _fetchCategories(
-      DatabaseExecutor executor) async {}
+      DatabaseExecutor executor) async {
+    final rawCategoryTables = await executor.query(CategoryTable.tableName);
+    return rawCategoryTables.map((raw) => CategoryTable.fromJson(raw)).toList();
+  }
 
   Future<void> _createCategory(
       DatabaseExecutor executor, CategoryTable category) async {
-    ///TODO
+    await executor.insert(CategoryTable.tableName, category.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
   Future<void> _updateCategory(
       DatabaseExecutor executor, CategoryTable category) async {
-    ///TODO
+    await executor.update(CategoryTable.tableName, category.toJson(),
+        where: '${CategoryTable.columnId} = ?',
+        whereArgs: <String>[category.id]);
   }
 
   Future<bool> _existsCategory(
       DatabaseExecutor executor, CategoryTable category) async {
-    ///TODO
+    final categories = await executor.query(CategoryTable.tableName,
+        where: '${CategoryTable.tableName} = ?',
+        whereArgs: <String>[category.id]);
+    return categories.isNotEmpty;
   }
 
   ///endregion
 
   ///region todos operation methods
-
   Future<List<TodoTable>> _fetchTodosOf(
-      DatabaseExecutor executor, String categoryId) async {}
+      DatabaseExecutor executor, String categoryId) async {
+    final rawTodoTables = await executor.query(TodoTable.tableName,
+        where: '${TodoTable.columnCategoryId} = ?',
+        whereArgs: <String>[categoryId]);
+    return rawTodoTables.map((raw) => TodoTable.fromJson(raw)).toList();
+  }
 
-  Future<void> _createTodo(DatabaseExecutor executor, TodoTable todo) async {}
+  Future<void> _createTodo(DatabaseExecutor executor, TodoTable todo) async {
+    await executor.insert(TodoTable.tableName, todo.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
 
-  Future<void> _updateTodo(DatabaseExecutor executor, TodoTable todo) async {}
+  Future<void> _updateTodo(DatabaseExecutor executor, TodoTable todo) async {
+    await executor.update(TodoTable.tableName, todo.toJson(),
+        where:
+            '${TodoTable.columnCategoryId} = ? and ${TodoTable.columnId} = ?',
+        whereArgs: <String>[todo.categoryId, todo.id]);
+  }
 
-  Future<bool> _existsTodo(DatabaseExecutor executor, TodoTable todo) async {}
+  Future<bool> _existsTodo(DatabaseExecutor executor, TodoTable todo) async {
+    final todos = await executor.query(TodoTable.tableName,
+        where: '${TodoTable.columnId} = ?', whereArgs: <String>[todo.id]);
+    return todos.isNotEmpty;
+  }
 
   ///endregion
 }
