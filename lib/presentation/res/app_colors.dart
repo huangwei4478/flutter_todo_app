07f46808f2d09cfc8e25f891faf18f0d5e45f174
huangwei <harw.op@qq.com>
2020-11-16 11:18:55 +0800

    new color scheme & fix andorid export

--- lib/presentation/res/app_colors.dart
+++ lib/presentation/res/app_colors.dart
@@ -3,25 +3,25 @@ import 'package:hexcolor/hexcolor.dart';
 
 // ignore: avoid_classes_with_only_static_members
 class AppColors {
-  static Color errorColor = HexColor('0xF27777');
+  static Color errorColor = HexColor('#F27777');
   static Color errorColorDark = errorColor;
 
-  static Color primaryColor = HexColor('0x535C5F');
+  static Color primaryColor = HexColor('#17E9E1');
   static Color primaryColorDark = primaryColor;
 
-  static Color accentColor = HexColor('0xF27777');
+  static Color accentColor = HexColor('#17E9E1');
   static Color accentColorDark = accentColor;
 
-  static Color textColor = HexColor('0x535C5F');
+  static Color textColor = HexColor('#17224D');
   static Color textColorDark = textColor;
 
-  static Color textSubColor = HexColor('0xDDDADB');
+  static Color textSubColor = HexColor('#17224D');
   static Color textSubColorDark = textSubColor;
 
-  static Color scaffoldBackgroundColor = HexColor('0xDDDAD8');
+  static Color scaffoldBackgroundColor = HexColor('#f5fafa');
   static Color scaffoldBackgroundColorDark = scaffoldBackgroundColor;
 
-  static Color toggleableActiveColor = HexColor('0x73939C');
+  static Color toggleableActiveColor = HexColor('#17224D');
   static Color toggleableActiveColorDark = toggleableActiveColor;
 
   static Color disabledColor = toggleableActiveColor;
